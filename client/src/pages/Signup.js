cc987eb23e67cca0320f12356eaa94b458703a41
Kenny <kenneththelarson@gmail.com>
2021-05-01 09:55:22 -0600

    user authentication

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/react-hooks';
+
 import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
@@ -24,7 +26,8 @@ const Signup = () => {
       const { data } = await addUser({
         variables: { ...formState }
       });
-      console.log(data);
+      
+      Auth.login(data.addUser.token);
     } catch (e) {
       console.error(e);
     }
